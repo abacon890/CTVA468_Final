8770da891d91b965857e10753c30c12d28188d2c
Gareth Bowler <bowlergareth@gmail.com>
2015-04-22 11:42:32 -0700

    shooting changes

--- pacman.js
+++ pacman.js
@@ -3,6 +3,10 @@
 			var pacman;
 			var loopTimer;
 			var numLoops = 0;
+			var bullets;
+			
+			var hSpeed;
+			var vSpeed;
 
 			var upArrowDown = false;
 			var downArrowDown = false;
@@ -38,7 +42,13 @@
 				loopTimer = setInterval(loop, 50);
 
 				
-				
+				bullets = document.createElement('DIV');
+				bullets.className = 'gameObject';
+				bullets.style.width = gameWindow.style.width;
+				bullets.style.height = gameWindow.style.height;
+				bullets.style.left = '0px';
+				bullets.style.top = '0px';
+				gameWindow.appendChild(bullets);
 
 
 				
@@ -70,22 +80,22 @@
 				var originalLeft = pacman.style.left;
 				var originalTop = pacman.style.top;
 				
-				if(direction=='up'){
+				if(upArrowDown){
 					var pacmanY = parseInt(pacman.style.top) - PACMAN_SPEED;
 					if(pacmanY < -30) pacmanY = 390;
 					pacman.style.top = pacmanY + 'px';
 				}
-				if(direction=='down'){
+				if(downArrowDown){
 					var pacmanY = parseInt(pacman.style.top) + PACMAN_SPEED;
 					if(pacmanY > 390) pacmanY = -30;
 					pacman.style.top = pacmanY + 'px';
 				}
-				if(direction=='left'){
+				if(leftArrowDown){
 					var pacmanX = parseInt(pacman.style.left) - PACMAN_SPEED;
 					if(pacmanX < -30) pacmanX = 590;
 					pacman.style.left = pacmanX + 'px';
 				}
-				if(direction=='right'){
+				if(rightArrowDown){
 					var pacmanX = parseInt(pacman.style.left) + PACMAN_SPEED;
 					if(pacmanX > 590) pacmanX = -30;
 					pacman.style.left = pacmanX + 'px';
@@ -95,7 +105,31 @@
 					pacman.style.top = originalTop;
 				}
 
-				
+				var b = bullets.children;
+				for(var i=0; i<b.length; i++){
+					
+						var newX = parseInt(b[i].style.left) - b[i].hSpeed;
+						var newY = parseInt(b[i].style.top) - b[i].vSpeed;
+					
+					if( newY < 0 ) bullets.removeChild(b[i]);
+					else{ 
+							b[i].style.top = newY + 'px';
+						// for(var j=0; j<enemies.length; j++){
+					// 		if( hittest(b[i], enemies[j]) ){
+// 								document.getElementById('sndExp').currentTime = 0;
+// 								document.getElementById('sndExp').play();
+// 								bullets.removeChild(b[i]);
+// 								explode(enemies[j]);
+// 								placeEnemyShip(enemies[j]);
+// 							}
+// 						} 
+						// output.innerHTML = autofireCount;
+					}
+					if( newX < 0 ) bullets.removeChild(b[i]);
+					else{ 
+							b[i].style.left = newX + 'px';
+					}
+				}
 				
 			}
 
@@ -129,7 +163,7 @@
 				if(leftArrowDown){ 
 					pacman.style.left = parseInt(pacman.style.left) - PACMAN_SPEED + 'px';
 					if( ! hitWall(pacman) ){ 
-						// leftArrowDown = true;
+						leftArrowDown = true;
 						direction = 'left';
 						pacman.className = "flip-horizontal";
 					}
@@ -137,15 +171,15 @@
 				if(upArrowDown){ 
 					pacman.style.top = parseInt(pacman.style.top) - PACMAN_SPEED + 'px';
 					if( ! hitWall(pacman) ){ 
-						// upArrowDown = true;
+					 upArrowDown = true;
 						direction = 'up';
 						pacman.className = "rotate270";
 					}
 				}
 				if(rightArrowDown){
 					pacman.style.left = parseInt(pacman.style.left) + PACMAN_SPEED + 'px';
-					if( ! hitWall(pacman) ){ 
-						// rightArrowDown = true;
+						 if( ! hitWall(pacman) ){ 
+						 rightArrowDown = true;
 						direction = 'right';
 						pacman.className = "";
 					}
@@ -153,12 +187,64 @@
 				if(downArrowDown){ 
 					pacman.style.top = parseInt(pacman.style.top) + PACMAN_SPEED + 'px';
 					if( ! hitWall(pacman) ){ 
-						// downArrowDown = true;
+						downArrowDown = true;
 						direction = 'down';
 						pacman.className = "rotate90";
+						
 					}
 				}
 
 				pacman.style.left = originalLeft;
 				pacman.style.top = originalTop;
 			}
+
+		function fire(){
+					// document.getElementById('sndShoot').currentTime = 0;
+// 					sndShoot = document.getElementById('sndShoot');
+// 					sndShoot.volume = 0.5;
+// 					sndShoot.play();
+
+					var bulletWidth = 4;
+					var bulletHeight = 10;
+					var bullet = document.createElement('DIV');
+					bullet.className = 'gameObject';
+					bullet.style.backgroundColor = 'black';
+					bullet.style.width = bulletWidth + 'px';
+					bullet.style.height = bulletHeight + 'px';
+					bullet.speed = 20;
+					// bullet.style.top = parseInt(pacman.style.top) - bulletHeight + 'px';
+					if(direction == 'left'){
+						bullet.hSpeed = 5;
+						bullet.vSpeed = 0;
+						bullet.style.height = '4px';
+						bullet.style.width = '10px';
+					}
+					if(direction == 'right'){
+						bullet.hSpeed = -5;
+						bullet.vSpeed = 0;
+						bullet.style.height = '4px';
+						bullet.style.width = '10px';
+					}
+					if(direction == 'up'){
+						bullet.hSpeed = 0;
+						bullet.vSpeed = 5;
+						
+					}
+					if(direction == 'down'){
+						bullet.hSpeed = 0;
+						bullet.vSpeed = -5;
+						
+					}
+					var pacmanX = parseInt(pacman.style.left) + parseInt(pacman.style.width)/2;
+					bullet.style.left = (pacmanX - bulletWidth/2) + 'px';
+					var pacmanY = parseInt(pacman.style.top) + parseInt(pacman.style.height)/2;
+					bullet.style.top = (pacmanY - bulletWidth/2) + 'px';
+					
+					bullets.appendChild(bullet);
+				}
+				
+			document.addEventListener('keypress', function(event){
+				// if(event.keyCode==32) fire();
+				if(event.charCode==32) fire();
+			});
+				
