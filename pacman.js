1096714d3d5cd409da65b470193d9628d44ce6a2
Garethab <bowlergareth@gmail.com>
2015-04-29 11:48:40 -0700

    Merge branch 'Walls' into Shooting
    Conflicts:
    	index.html
    	pacman.js
    	style.css

--- pacman.js
+++ pacman.js
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 			var output; 
 			var pacman;
 			var loopTimer;
@@ -42,10 +43,18 @@
 				pacman.style.top = '240px';
 				pacman.style.width = '40px';
 				pacman.style.height = '40px';
+=======
+var output; 
+var pacman;
+var loopTimer;
+var numLoops = 0;
+var bullets;
+>>>>>>> Walls
 
-				
-				loopTimer = setInterval(loop, 50);
+var hSpeed;
+var vSpeed;
 
+<<<<<<< HEAD
 				
 				bullets = document.createElement('DIV');
 				bullets.className = 'gameObject';
@@ -54,7 +63,15 @@
 				bullets.style.left = '0px';
 				bullets.style.top = '0px';
 				gameWindow.appendChild(bullets);
+=======
+var upArrowDown = false;
+var downArrowDown = false;
+var leftArrowDown = false;
+var rightArrowDown = false;
+var direction = 'right';
+>>>>>>> Walls
 
+<<<<<<< Updated upstream
 				ghosts[0] = document.getElementById('redGhost');
 				ghosts[1] = document.getElementById('blueGhost');
 				ghosts[2] = document.getElementById('greenGhost');
@@ -70,6 +87,17 @@
 
 
 			
+=======
+var walls = new Array();
+
+<<<<<<< HEAD
+				ghost = document.getElementById('whiteGhost');
+				
+				ghost.style.left = '280px';
+				ghost.style.top = '240px';
+				ghost.style.width = '40px';
+				ghost.style.height = '40px';
+>>>>>>> Stashed changes
 
 				
 				// top wall 
@@ -83,29 +111,24 @@
 				// top wall
 				createWall(-20, 360, 640, 40);
 			}
+=======
+var redGhost;
+var blueGhost;
+var greenGhost;
+var pinkGhost;
+>>>>>>> Walls
 
-			function createWall(left, top, width, height){
-				var wall = document.createElement('div');
-				wall.className = 'wall';
-				wall.style.left =  left + 'px';
-				wall.style.top =  top + 'px';
-				wall.style.width = width + 'px';
-				wall.style.height =  height + 'px';
-				gameWindow.appendChild(wall);
+var rgDirection; // Red Ghost Direction
+var bgDirection;
+var ggDirection;
+var pgDirection;
+var gameWindowH = 400;
+var gameWindowW = 600;
 
-				// var numWalls = walls.length;
-				// walls[numWalls] = wall;
-				// var numWalls = walls.length;
-				// walls[numWalls] = wall;
-				walls.push(wall);
-				// output.innerHTML = walls.length;
+const PACMAN_SPEED = 10;
+const GHOST_SPEED = 5;
 
-			}
-			function loop(){
-				// output.innerHTML = direction;
-				numLoops++;
-				tryToChangeDirection();
-
+<<<<<<< HEAD
 				var originalLeft = pacman.style.left;
 				var originalTop = pacman.style.top;
 				
@@ -128,11 +151,141 @@
 					var pacmanX = parseInt(pacman.style.left) + PACMAN_SPEED;
 					if(pacmanX > gameWindowW - 10) pacmanX = -30;
 					pacman.style.left = pacmanX + 'px';
+=======
+function init(){
+	// alert('page loaded');
+	output = document.getElementById('output');
+	pacman = document.getElementById('pacman');
+	pacman.style.left = '280px';
+	pacman.style.top = '240px';
+	pacman.style.width = '40px';
+	pacman.style.height = '40px';
+
+	
+	loopTimer = setInterval(loop, 50);
+
+	
+	bullets = document.createElement('DIV');
+	bullets.className = 'gameObject';
+	bullets.style.width = gameWindow.style.width;
+	bullets.style.height = gameWindow.style.height;
+	bullets.style.left = '0px';
+	bullets.style.top = '0px';
+	gameWindow.appendChild(bullets);
+
+	
+	createWall(240, 200, 120, 40);
+	createWall(240, 280, 120, 40);
+	createWall(80, 160, 40, 160);
+	createWall(480, 160, 40, 160);
+	createWall(160, 240, 40, 160);
+	createWall(160, 0, 40, 120);
+	createWall(400, 240, 40, 160);
+	createWall(400, 0, 40, 120);
+	createWall(80, 80, 40, 40);
+	createWall(480, 80, 40, 40);
+	createWall(160, 160, 40, 40);
+	createWall(400, 160, 40, 40);
+	createWall(240, 80, 40, 80);
+	createWall(320, 80, 40, 80);
+}
+
+function createWall(left, top, width, height){
+	var wall = document.createElement('div');
+	wall.className = 'wall';
+	wall.style.left = left + 'px';
+	wall.style.top = top + 'px';
+	wall.style.width = width + 'px';
+	wall.style.height = height + 'px';
+	gameWindow.appendChild(wall);
+
+	walls.push(wall);
+}
+
+function loop(){
+	// output.innerHTML = direction;
+	numLoops++;
+	tryToChangeDirection();
+
+	var originalLeft = pacman.style.left;
+	var originalTop = pacman.style.top;
+
+
+	
+	if(upArrowDown){
+		var pacmanY = parseInt(pacman.style.top) - PACMAN_SPEED;
+		if(pacmanY < -30) pacmanY = gameWindowH - 10;
+		pacman.style.top = pacmanY + 'px';
+	}
+	if(downArrowDown){
+		var pacmanY = parseInt(pacman.style.top) + PACMAN_SPEED;
+		if(pacmanY > gameWindowH - 10) pacmanY = -30;
+		pacman.style.top = pacmanY + 'px';
+	}
+	if(leftArrowDown){
+		var pacmanX = parseInt(pacman.style.left) - PACMAN_SPEED;
+		if(pacmanX < -30) pacmanX = gameWindowW - 10;
+		pacman.style.left = pacmanX + 'px';
+	}
+	if(rightArrowDown){
+		var pacmanX = parseInt(pacman.style.left) + PACMAN_SPEED;
+		if(pacmanX > gameWindowW - 10) pacmanX = -30;
+		pacman.style.left = pacmanX + 'px';
+	}
+	if(hitWall(pacman) ){
+		pacman.style.left = originalLeft;
+		pacman.style.top = originalTop;
+	}
+
+	var b = bullets.children;
+	for(var i=0; i<b.length; i++){
+		
+		var newX = parseInt(b[i].style.left) - b[i].hSpeed;
+		var newY = parseInt(b[i].style.top) - b[i].vSpeed;
+		
+		if(b[i].vSpeed > 0){
+			// BULLET MOVING UP
+			if( newY > gameWindowH ) bullets.removeChild(b[i]);
+			else{ 
+				b[i].style.top = newY + 'px';
+				if(hitWall(b[i]) ){
+					bullets.removeChild(b[i]);
 				}
-				if(hitWall(pacman) ){
-					pacman.style.left = originalLeft;
-					pacman.style.top = originalTop;
+			}						
+		}
+		else if(b[i].vSpeed < 0){
+			// BULLET MOVING DOWN
+			if( newY < 0 ) bullets.removeChild(b[i]);
+			else{ 
+				b[i].style.top = newY + 'px';
+				if(hitWall(b[i]) ){
+					bullets.removeChild(b[i]);
+>>>>>>> Walls
 				}
+			}	
+		}	
+			
+		
+		else if(b[i].hSpeed < 0){
+			// BULLET MOVING LEFT
+			if( newX < 0 ) bullets.removeChild(b[i]);
+			else{ 
+				b[i].style.left = newX + 'px';
+					if(hitWall(b[i]) ){
+					bullets.removeChild(b[i]);
+				}	
+			}
+		}
+		else if(b[i].hSpeed > 0){
+			// BULLET MOVING RIGHT
+			if( newX > gameWindowW ) bullets.removeChild(b[i]);
+			else{
+				b[i].style.left = newX + 'px';
+			
+				if(hitWall(b[i]) ){
+					bullets.removeChild(b[i]);
+				}
+<<<<<<< HEAD
 
 				moveGhosts();
 
@@ -181,32 +334,42 @@
 						}
 					}
 				}
+=======
+>>>>>>> Walls
 				
 			}
+		}
+	}	
+}
 
-			
-			document.addEventListener('keydown', function(event){
-				if(event.keyCode==37) leftArrowDown = true;
-				if(event.keyCode==38) upArrowDown = true;
-				if(event.keyCode==39) rightArrowDown = true;
-				if(event.keyCode==40) downArrowDown = true;
-			});
 
-			document.addEventListener('keyup', function(event){
-				if(event.keyCode==37) leftArrowDown = false;
-				if(event.keyCode==38) upArrowDown = false;
-				if(event.keyCode==39) rightArrowDown = false;
-				if(event.keyCode==40) downArrowDown = false;
-			});
+document.addEventListener('keydown', function(event){
+	if(event.keyCode==37) leftArrowDown = true;
+	if(event.keyCode==38) upArrowDown = true;
+	if(event.keyCode==39) rightArrowDown = true;
+	if(event.keyCode==40) downArrowDown = true;
+});
 
-			function hitWall(element){
-				var hit = false;
-				for(var i=0; i<walls.length; i++){
-					if( hittest(walls[i], element) ) hit = true;
-				}
-				return hit;
-			}
+document.addEventListener('keyup', function(event){
+	if(event.keyCode==37) leftArrowDown = false;
+	if(event.keyCode==38) upArrowDown = false;
+	if(event.keyCode==39) rightArrowDown = false;
+	if(event.keyCode==40) downArrowDown = false;
+});
+document.addEventListener('keypress', function(event){
+	// if(event.keyCode==32) fire();
+	if(event.charCode==32) fire();
+});
 
+function hitWall(element){
+	var hit = false;
+	for(var i=0; i<walls.length; i++){
+		if( hittest(walls[i], element) ) hit = true;
+	}
+	return hit;
+}
+
+<<<<<<< Updated upstream
 			function moveGhosts(){
 				for(var i=0; i<ghosts.length; i++){
 	
@@ -338,6 +501,47 @@
 	}
 }
 
+=======
+function tryToChangeDirection(){
+var originalLeft = pacman.style.left;
+var originalTop = pacman.style.top;
+
+	if(leftArrowDown){ 
+		pacman.style.left = parseInt(pacman.style.left) - PACMAN_SPEED + 'px';
+		if( ! hitWall(pacman) ){ 
+			leftArrowDown = true;
+			direction = 'left';
+			pacman.className = "flip-horizontal";
+		}
+	}
+	if(upArrowDown){ 
+		pacman.style.top = parseInt(pacman.style.top) - PACMAN_SPEED + 'px';
+		if( ! hitWall(pacman) ){ 
+		 upArrowDown = true;
+			direction = 'up';
+			pacman.className = "rotate270";
+		}
+	}
+	if(rightArrowDown){
+		pacman.style.left = parseInt(pacman.style.left) + PACMAN_SPEED + 'px';
+			 if( ! hitWall(pacman) ){ 
+			 rightArrowDown = true;
+			direction = 'right';
+			pacman.className = "";
+		}
+	}
+	if(downArrowDown){ 
+		pacman.style.top = parseInt(pacman.style.top) + PACMAN_SPEED + 'px';
+		if( ! hitWall(pacman) ){ 
+			downArrowDown = true;
+			direction = 'down';
+			pacman.className = "rotate90";
+			
+		}
+	}
+
+<<<<<<< HEAD
+>>>>>>> Stashed changes
 			function tryToChangeDirection(){
 			var originalLeft = pacman.style.left;
 			var originalTop = pacman.style.top;
@@ -382,10 +586,19 @@
 
 		function fire(){
 					// document.getElementById('sndShoot').currentTime = 0;
+=======
+	pacman.style.left = originalLeft;
+	pacman.style.top = originalTop;
+}
+
+function fire(){
+		// document.getElementById('sndShoot').currentTime = 0;
+>>>>>>> Walls
 // 					sndShoot = document.getElementById('sndShoot');
 // 					sndShoot.volume = 0.5;
 // 					sndShoot.play();
 
+<<<<<<< HEAD
 					var bulletWidth = 4;
 					var bulletHeight = 10;
 					var bullet = document.createElement('DIV');
@@ -430,3 +643,46 @@
 				if(event.charCode==32) fire();
 			});
 				
+=======
+		var bulletWidth = 4;
+		var bulletHeight = 10;
+		var bullet = document.createElement('DIV');
+		bullet.className = 'gameObject';
+		bullet.style.backgroundColor = 'black';
+		bullet.style.width = bulletWidth + 'px';
+		bullet.style.height = bulletHeight + 'px';
+		bullet.speed = 1;
+		// bullet.style.top = parseInt(pacman.style.top) - bulletHeight + 'px';
+		if(direction == 'left'){
+			bullet.hSpeed = PACMAN_SPEED + bullet.speed;
+			bullet.vSpeed = 0;
+			bullet.style.height = '4px';
+			bullet.style.width = '10px';
+		}
+		if(direction == 'right'){
+			bullet.hSpeed = (PACMAN_SPEED*-1) -bullet.speed;
+			bullet.vSpeed = 0;
+			bullet.style.height = '4px';
+			bullet.style.width = '10px';
+		}
+		if(direction == 'up'){
+			bullet.hSpeed = 0;
+			bullet.vSpeed = PACMAN_SPEED + bullet.speed;
+			
+		}
+		if(direction == 'down'){
+			bullet.hSpeed = 0;
+			bullet.vSpeed = (PACMAN_SPEED*-1) -bullet.speed;
+			
+		}
+		var pacmanX = parseInt(pacman.style.left) + parseInt(pacman.style.width)/2;
+		bullet.style.left = (pacmanX - bulletWidth/2) + 'px';
+		var pacmanY = parseInt(pacman.style.top) + parseInt(pacman.style.height)/2;
+		bullet.style.top = (pacmanY - bulletWidth/2) + 'px';
+		
+		bullets.appendChild(bullet);
+	}
+	
+
+	
+>>>>>>> Walls
